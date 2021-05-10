284ad4f4587c64777b5888598bc818b25d98047a
Rafae1CM <rafaelcezarmachado@outlook.com>
2021-05-10 16:10:57 -0300

    🎋 Listener registry.

---
+++ src/structures/listener/ListenerRegistry.ts
@@ -0,0 +1,20 @@
+import Client from "../Client";
+
+import Listener from "./Listener";
+import Registry from "../registry/Registry";
+
+export default class ListenerRegistry extends Registry {
+
+    constructor (client: Client) {
+        super();
+
+        this.client = client;
+        
+        this.events.on("added", (module: Listener) => {
+            this.client.on(module.name, (...args) => module.run(this.client, ...args));
+        });
+    };
+
+    public client: Client;
+
+};
