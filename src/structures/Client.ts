9d7670503e9639847956e91dc5391edcc9cea297
Rafae1CM <rafaelcezarmachado@outlook.com>
2021-05-10 16:14:24 -0300

    🥳 Eris client structure file.

--- src/structures/Client.ts
+++ src/structures/Client.ts
@@ -1,3 +1,21 @@
 import { Client as ErisClient } from "eris";
+import { ClientOptions } from "eris";
 
-export default class Client extends ErisClient {};
+import ListenerRegistry from "./listener/ListenerRegistry";
+
+export default class Client extends ErisClient {
+
+    constructor (token: string, options?: ClientOptions) {
+        super(token, options);
+
+        this.listenerRegistry = new ListenerRegistry(this);
+    };
+
+    public listenerRegistry: ListenerRegistry;
+
+    public async start() {
+        await super.connect();
+        this.listenerRegistry.loadPath("./src/listeners");
+    };
+
+};
