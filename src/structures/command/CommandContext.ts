d4cdb34c24ae96bfb59d48b8b5a0aa9951a3b006
Rafae1CM <rafaelcezarmachado@outlook.com>
2021-05-10 17:58:36 -0300

    👗 Command context structure.

--- src/structures/command/CommandContext.ts
+++ src/structures/command/CommandContext.ts
@@ -2,16 +2,18 @@ import Client from "../Client";
 
 export default class CommandContext {
 
-    constructor (client: Client, message: any, args: string[], locale: any) {
+    constructor (client: Client, message: any, args: string[], locale: any, database: { user: any, guild: any }) {
         this.client = client;
         this.message = message;
         this.args = args;
         this.locale = locale;
+        this.database = database;
     };
 
     public client: Client;
     public message: any;
     public args: string[];
     public locale: any;
+    public database: { user: any, guild: any };
 
 };
