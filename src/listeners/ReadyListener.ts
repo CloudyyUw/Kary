7d26d4ac80337654fa601588278f64cb7aebc2ae
Rafae1CM <rafaelcezarmachado@outlook.com>
2021-05-10 17:40:18 -0300

    🧦 Client ready listener.

--- src/listeners/ReadyListener.ts
+++ src/listeners/ReadyListener.ts
@@ -1,10 +1,11 @@
 import Listener from "../structures/listener/Listener";
+import Client from "../structures/Client";
 import Logger from "../structures/util/Logger";
 
 export default class ReadyListener extends Listener {
 
     public name: string = "ready";
-    public run(client) {
+    public run(client: Client) {
         Logger.text(`Success in connecting with Discord.`);
     };
 
