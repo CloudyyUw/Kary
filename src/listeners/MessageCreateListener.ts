57a2bf9b073a81ce9fab02a222af60508e4c5a58
Rafae1CM <rafaelcezarmachado@outlook.com>
2021-05-11 12:58:46 -0300

    verify bot and user permissions

--- src/listeners/MessageCreateListener.ts
+++ src/listeners/MessageCreateListener.ts
@@ -35,9 +35,28 @@ export default class MessageCreateListener extends Listener {
         };
     };
 
+    private verifyPermissions(message: any, memberId: string, permissions: string[]): any {
+        const missingPermissions = [];
+        const memberPermissions = message.channel.guild.members.get(memberId).permissions;
+        permissions.forEach(permission => {
+            if ( !memberPermissions.has(permission) ) missingPermissions.push(permission);
+        });
+        return missingPermissions;
+    };
+
+    private missingBotPermissions(message: any, client: any) {
+        const necessaryPermissions = ["sendMessages", "readMessageHistory"];
+        if ( this.verifyPermissions(message, client.user.id, necessaryPermissions).length > 0 ) {
+            return false;
+        } else {
+            return true;
+        };
+    };
+
     public name: string = "messageCreate";
     public async run(client: Client, message: any) {
         if ( message.author.bot || message.webhookID || !message.guildID ) return;
+        if ( !this.missingBotPermissions(message, client) ) return;
 
         const guildData = this.getGuildData(message.guildID);
         const userData = this.getUserData(message.author.id);
@@ -54,6 +73,22 @@ export default class MessageCreateListener extends Listener {
 
         const context = new CommandContext(client, message, args, locale, { guild: guildData, user: userData });
         await message.channel.sendTyping();
+
+        if ( command?.botPermission != [] ) {
+            let missingPermissions = this.verifyPermissions(message, client.user.id, command.botPermission);
+            if ( missingPermissions.length > 0 ) {
+                missingPermissions = missingPermissions.map(v => locale(`permissions:${v}`)).join(", ");
+                return context.replyT("Error", "basic:missingBotPermission", { permissions: missingPermissions });
+            };
+        };
+
+        if ( command?.userPermission != [] ) {
+            let missingPermissions = this.verifyPermissions(message, client.user.id, command.userPermission);
+            if ( missingPermissions.length > 0 ) {
+                missingPermissions = missingPermissions.map(v => locale(`permissions:${v}`)).join(", ");
+                return context.replyT("Error", "basic:missingUserPermission", { permissions: missingPermissions });
+            };
+        };
 
         if ( command?.onlyDevelopers != false ) {
             if ( !process.env.DEVELOPERS.includes(message.author.id) ) {
