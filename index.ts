bd79f6d22955327aeb635627a9c9016523353114
Rafae1CM <rafaelcezarmachado@outlook.com>
2021-05-13 07:56:29 -0300

    client options

--- index.ts
+++ index.ts
@@ -2,12 +2,9 @@ import Client from "./src/structures/Client";
 import dotenv from "dotenv";
 
 dotenv.config();
-const client = new Client(process.env.TOKEN, {
-    restMode: true,
-    allowedMentions: {
-        repliedUser: true,
-        everyone: false,
-    },
-});
+const client = new Client(process.env.TOKEN, { restMode: true, allowedMentions: {
+    repliedUser: true,
+    everyone: false,
+}});
 
 client.start();
